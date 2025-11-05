465b738c11efed8de372d46f14eb64345db68e88
ganendrpratama <ganendra1@gmail.com>
2025-11-05 19:43:55 +0700

    FIX: Predict.js can contact the API now

--- Client App/frontend/src/services/predict.js
+++ Client App/frontend/src/services/predict.js
@@ -1,9 +1,9 @@
 /**
  * Property Price Prediction Service
- * Connects to localhost:8000/prediction API endpoint
+ * Connects to localhost:8000/predict API endpoint
  */
 
-const API_URL = 'http://localhost:8000/prediction'
+const API_URL = 'http://localhost:8000/predict'
 const DEFAULT_TIMEOUT = 30000 // 30 seconds for ML prediction
 
 /**
